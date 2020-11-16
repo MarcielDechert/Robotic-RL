d8a9d49458eac7722948043254aeeca223f65fdc
RickWolff95 <73991378+RickWolff95@users.noreply.github.com>
2020-11-16 15:54:20 +0100

    5-Achsen-Roboter
    Abwurfgebiet hinzugefügt

--- Robotic-RL-Env/Assets/Script/IKManagerRobot.cs
+++ Robotic-RL-Env/Assets/Script/IKManagerRobot.cs
@@ -9,11 +9,16 @@ public class IKManagerRobot : MonoBehaviour
     [SerializeField] private AchseV1 r_Ausgangsachse;
     [SerializeField] private AchseV1 r_Endpunkt;
     [SerializeField] private GameObject r_Zielpunkt;
+    [SerializeField] private Rigidbody r_Ball;
 
     [SerializeField] private float r_Drehrate = 5.0f;
     [SerializeField] private float r_Schwelle = 0.05f;
     [SerializeField] private int r_Schritte = 20;
-    
+
+    private void Start()
+    {
+        //r_Ball = GetComponent<Rigidbody>();
+    }
 
     float BerechneSteigung(AchseV1 _achse)
     {
@@ -51,6 +56,10 @@ public class IKManagerRobot : MonoBehaviour
                     // holt sich die nächste Achse/Verbindungspunkt
                     aktuelleAchse = aktuelleAchse.GetChild();
                 }
+            }
+            else
+            {
+                r_Ball.useGravity = true;
             }
 
         }

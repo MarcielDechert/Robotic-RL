7bca11bfd173024474c3533f00f338eddf598ef1
RickWolff95 <73991378+RickWolff95@users.noreply.github.com>
2021-03-10 19:23:01 +0100

    FinaleVersion

---
+++ Robotic-RL-Env/Assets/Script/BasisSkript/IStep.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stellt eine Methode bereit die für die Synchronisation zuständig sein soll
+/// </summary>
+public interface IStep
+{
+    void Step();
+}
